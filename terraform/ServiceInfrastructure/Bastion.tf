489121682f18a6c805fa92f8e3bcf4de43abcd47
Mark-Quark <siegmund@csun.edu>
2018-09-19 13:55:33 -0700

    addedVarsAndNotes

--- terraform/ServiceInfrastructure/Bastion.tf
+++ terraform/ServiceInfrastructure/Bastion.tf
@@ -1,14 +1,28 @@
 #Bastion
 
 resource "aws_instance" "bastion" {
-  ami                         = "ami-04681a1dbd79675a5"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  #key_name = "${var.key_name}"
-  #vpc_security_group_ids=["sg-7cad580b","sg-91edb3e4"]
-  subnet_id="subnet-022378249a6d9694f"
-tags {
-      Name = "Bastion Host"
-  }
 
+  #redhat linux 
+  ami                         = "ami-6871a115" 
+  
+  instance_type               = "t2.micro"
+  
+  associate_public_ip_address = true
+  
+  # The name of our SSH keypair
+  key_name                    = "${var.aws_key_name}"
+  
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids     =  ["${data.aws_security_group.bastionhostsg.id}"]
+  
+  subnet_id                   = "${data.aws_subnet.public_sub1.id}"
+  Type                        = "BastionHost"
+  tags {
+      Name = "Bastion Host"
+       }
+
+}
+
+output "bastion_host_public ip" {
+  value = "${aws_instance.bastion_host.public_ip}"
 }
