cba4f018785b7bc9989682d8bf9031ce96a5e35b
aubreynigoza <aubrey.nigoza.34@my.csun.edu>
2018-09-29 07:21:31 -0700

    adjusting route53

--- terraform/ServiceInfrastructure/route53.tf
+++ terraform/ServiceInfrastructure/route53.tf
@@ -24,3 +24,15 @@ resource "aws_route53_record" "apex" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "blog" {
+  zone_id = "${data.aws_route53_zone.sandboxworms_zone.id}"
+  name    = "blog"
+  type    = "A"
+  #ttl     = "300"
+    alias {
+    name                   = "${aws_alb.sandbox-ALB.dns_name}"
+    zone_id                = "${aws_alb.sandbox-ALB.zone_id}"
+    evaluate_target_health = true
+  }
+}
