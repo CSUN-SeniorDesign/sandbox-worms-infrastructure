ef38a33d7483df80794a1a3ae30532b2485a6539
aubreynigoza <aubrey.nigoza.34@my.csun.edu>
2018-10-03 13:08:17 -0700

    took care of prod

--- ansible/proddeploy.sh
+++ ansible/proddeploy.sh
@@ -7,7 +7,7 @@ echo $PWD
 if [ -f "$file" ]
 then
         echo "$file found."
-    aws s3api get-object --bucket sandboxworms-packages-92618 --key staging/latestbuild.txt myprodlatestbuild.txt
+    aws s3api get-object --bucket sandboxworms-packages-92618 --key master/latestbuild.txt myprodlatestbuild.txt
     result=$(diff prodlatestbuild.txt myprodlatestbuild.txt)
     if [ $? -eq 0 ]
     then
@@ -17,15 +17,15 @@ then
             echo "files are different"
             value=$(<myprodlatestbuild.txt)
             echo "$value"
-            aws s3api get-object --bucket sandboxworms-packages-92618 --key staging/"$value" "$value"
+            aws s3api get-object --bucket sandboxworms-packages-92618 --key master/"$value" "$value"
             rm prodlatestbuild.txt
             mv myprodlatestbuild.txt prodlatestbuild.txt
     fi
 else
-        aws s3api get-object --bucket sandboxworms-packages-92618 --key staging/latestbuild.txt prodlatestbuild.txt
+        aws s3api get-object --bucket sandboxworms-packages-92618 --key master/latestbuild.txt prodlatestbuild.txt
         value=$(<prodlatestbuild.txt)
         echo "$value"
-        aws s3api get-object --bucket sandboxworms-packages-92618 --key staging/"$value" "$value"
+        aws s3api get-object --bucket sandboxworms-packages-92618 --key master/"$value" "$value"
 fi
 
 sudo tar -xzf "$value" -C /var/www/vhosts/prod_sandboxworms
