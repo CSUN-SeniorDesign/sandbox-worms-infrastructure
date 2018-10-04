b25ad6d2d86fd435b2b857cac1a8501a469f3064
aubreynigoza <aubrey.nigoza.34@my.csun.edu>
2018-10-03 21:05:05 -0700

    test

---
+++ ansible/deploysite.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+file="latestbuild.txt"
+if [ -f "$file" ]
+then
+	echo "$file found."
+    aws s3api get-object --bucket sandboxworms-packages-92618 --key staging/latestbuild.txt mylatestbuild.txt
+    result=$(diff latestbuild.txt mylatestbuild.txt)
+    if [ $? -eq 0 ]
+    then
+            echo "files are the same"
+            exit 1
+    else
+            echo "files are different"
+            value=$(<mylatestbuild.txt)
+            echo "$value"
+            aws s3api get-object --bucket sandboxworms-packages-92618 --key staging/"$value" "$value"
+            rm latestbuild.txt
+            mv mylatestbuild.txt latestbuild.txt
+    fi
+else
+        aws s3api get-object --bucket sandboxworms-packages-92618 --key staging/latestbuild.txt latestbuild.txt
+        value=$(<latestbuild.txt)
+        echo "$value"
+        aws s3api get-object --bucket sandboxworms-packages-92618 --key staging/"$value" "$value"
+fi
+
+sudo tar -xzf "$value" -C /var/www/vhosts/staging_sandboxworms
+sudo chown -R apache:apache  /var/www/vhosts/staging_sandboxworms
