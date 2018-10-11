455a7bebd3eade47f5c932b0cd921585f79566cb
jpvinuya <johnphilip.vinuya.778@my.csun.edu>
2018-10-10 17:02:37 -0700

    ECS assume role issue

--- terraform/ServiceInfrastructure/ECS.tf
+++ terraform/ServiceInfrastructure/ECS.tf
@@ -1,6 +1,7 @@
-data "aws_ecs_task_definition" "cluster-task" {
-  task_definition = "${aws_ecs_task_definition.cluster-task.family}"
+data "aws_ecs_task_definition" "clustertask" {
+  task_definition = "${aws_ecs_task_definition.clustertask.family}"
 }
+
 
 #===================CLUSTER==================
 resource "aws_ecs_cluster" "SBW-terraform-cluster" {
@@ -8,8 +9,8 @@ resource "aws_ecs_cluster" "SBW-terraform-cluster" {
 }
 
 #===================TASK DEFINITION==================
-resource "aws_ecs_task_definition" "cluster-task" {
-  family                = "cluster-task"
+resource "aws_ecs_task_definition" "clustertask" {
+  family                = "clustertask"
   container_definitions = "${file("task-definitions/service.json")}"
   
   placement_constraints {
@@ -17,21 +18,22 @@ resource "aws_ecs_task_definition" "cluster-task" {
     expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
   }
   
-#  task_role_arn			=	"arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-  
-  execution_role_arn	=	"arn:aws:iam::429784283093:role/ecsTaskExecutionRole"  
+#  task_role_arn		=	"arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role" 
+#  execution_role_arn	=	"arn:aws:iam::429784283093:role/ecsTaskExecutionRole"  
 }
 
 #===================SERVICE==================
 resource "aws_ecs_service" "SBW-Cluster-ALB" {
   name            = "SBW-Cluster-ALB"
   cluster         = "${aws_ecs_cluster.SBW-terraform-cluster.id}"
-  task_definition = "${aws_ecs_task_definition.cluster-task.family}:${max("${aws_ecs_task_definition.cluster-task.revision}", "${data.aws_ecs_task_definition.cluster-task.revision}")}"
+#  task_definition = "aws_ecs_task_definition.clustertask.arn"
+  task_definition = "${aws_ecs_task_definition.clustertask.family}:${max("${aws_ecs_task_definition.clustertask.revision}", "${data.aws_ecs_task_definition.clustertask.revision}")}"
   desired_count   = 3
-  iam_role        = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+  iam_role        = "arn:aws:iam::429784283093:role/ecsTaskExecutionRole"
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.sandbox-target.arn}"
+#    target_group_arn = "aws_lb_target_group.sandbox-target.arn"
+	target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:429784283093:targetgroup/ecs-SBW-Cl-SB-ALB/da32f7059dd79168"
     container_name   = "cluster-task"
     container_port   = 80
   }
@@ -41,7 +43,21 @@ resource "aws_ecs_service" "SBW-Cluster-ALB" {
     expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
   }
 }
+/*
+resource "aws_ecs_service" "test-ecs-service" {
+  	name            = "test-ecs-service"
+  	iam_role        = "arn:aws:iam::429784283093:role/ecsTaskExecutionRole"
+  	cluster         = "${aws_ecs_cluster.SBW-terraform-cluster.id}"
+  	task_definition = "${aws_ecs_task_definition.clustertask.family}:${max("${aws_ecs_task_definition.clustertask.revision}", "${data.aws_ecs_task_definition.clustertask.revision}")}"
+  	desired_count   = 2
 
+  	load_balancer {
+    	target_group_arn  = "aws_alb_target_group.sandbox-target.arn"
+    	container_port    = 80
+    	container_name    = "wordpress"
+	}
+}
+*/
 #===================ECR REPO==================
 /*
 resource "aws_ecr_repository" "SBW-repo" {
